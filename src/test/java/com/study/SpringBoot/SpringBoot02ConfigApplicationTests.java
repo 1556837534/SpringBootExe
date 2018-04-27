f47bcf0ceb5451761950ba8d712a586efb9561ce
q <1556837534@qq.com>
2018-04-27 21:14:44 +0800

    Spring Boot 第一阶段 自动配置的原理

--- src/test/java/com/study/SpringBoot/SpringBoot02ConfigApplicationTests.java
+++ src/test/java/com/study/SpringBoot/SpringBoot02ConfigApplicationTests.java
@@ -2,6 +2,8 @@ package com.study.SpringBoot;
 import com.study.SpringBoot.beans.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -18,7 +20,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBoot02ConfigApplicationTests {
-
+	Logger logger = LoggerFactory.getLogger(getClass()); //日志记录器
 	@Autowired
 	Person person;
 
@@ -29,6 +31,18 @@ public class SpringBoot02ConfigApplicationTests {
 	public void testHelloService(){
 		boolean b = ioc.containsBean("helloService02");
 		System.out.println(b);
+	}
+	//测试SpringBoot日志
+	@Test
+	public void testLogging(){
+		//日志级别  由低到高 trace《debug《info《warn《error
+		//可以调整日志的输出级别  日志就会只会在这个级别与以后的高级别打印
+		logger.trace("这是trace日志");
+		logger.debug("这是debug 日志");
+		//SpringBoot 默认使用的日志级别是 info级别的
+		logger.info("这是info日志");
+		logger.warn("这是warn警告日志");
+		logger.error("这是异常日志");
 	}
 
 
