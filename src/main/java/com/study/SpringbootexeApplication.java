2e92f8fb152e78b05952eb3f36e66489962bc5a6
q <1556837534@qq.com>
2018-04-27 22:04:37 +0800

    SpringBoot Web 练习

---
+++ src/main/java/com/study/SpringbootexeApplication.java
@@ -0,0 +1,12 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootexeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootexeApplication.class, args);
+	}
+}
